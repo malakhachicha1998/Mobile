295458a21a58464ba0bee3287cd33bd7fb101ae6
malakhachicha1998 <malak.hachicha@esprit.tn>
2023-11-12 22:47:54 +0100

    entity user modified

--- app/src/main/java/com/example/engineera/Data/Entities/User.java
+++ app/src/main/java/com/example/engineera/Data/Entities/User.java
@@ -12,4 +12,28 @@ public class User {
     private String email;
     @ColumnInfo(name = "password")
     private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
